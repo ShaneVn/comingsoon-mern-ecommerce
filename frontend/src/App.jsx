408275bba3d81decc24deac4c3f51f0d61d64bb5
Shane <shaneboo152@gmail.com>
2021-12-05 14:49:32 -0800

    development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,43 +4,66 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
 import CartScreen from "./screens/CartScreen";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SigninScreen from "./screens/signinScreen";
+import { signout } from "./actions/userAction";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
-  const cart = useSelector(state=>state.cart)
-  const {cartItems} = cart
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const dispatch = useDispatch()
+  const signoutHandler = ()=>{
+    dispatch(signout())
+  }
 
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-        <Link className="brand" to ='/'>
-            amazona
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              amazona
             </Link>
-        </div>
-        <div>
-          <Link to='/cart'>Cart
-          {cartItems.length > 0 && (<span className= "badge">{cartItems.length}</span>)}
-          </Link>
-          <Link to='/signin'>Sign In</Link>
-        </div>
-      </header>
-      <main>
-       
-        <Routes>
-          <Route  path="/cart/:id" element= {<CartScreen/>}/>
-        <Route path= "/product/:id" element={<ProductScreen/>}/>
-        <Route path ="/signin" element = {<SigninScreen/>}/>
-        <Route exact path = "/" element = {<HomeScreen/>}/>
-        </Routes>
-         
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
-
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className = "dropdown">
+              <Link to="#"> {userInfo.name}
+              <i className="fa fa-caret-down"></i>
+              </Link>
+              <ul className = "dropdown-content">
+                <li>
+                  <Link to ="#signout" onClick={signoutHandler}>
+                    Sign out
+                  </Link>
+                </li>
+              </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/cart/:id" element={<CartScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/signin" element={<SigninScreen />} />
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </BrowserRouter>
   );
 }
--- frontend/src/screens/signinScreen.jsx
+++ frontend/src/screens/signinScreen.jsx
@@ -1,25 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signin } from "../actions/userAction";
+import { useLocation, useNavigate  } from "react-router";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
 
 export default function SigninScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-
+  const location = useLocation()
   const dispatch = useDispatch()
+
+  const redirect = location.search? location.search.split('=')[1] : '/'
+  const userSignin = useSelector((state)=> state.userSignin)
+  const {userInfo, loading, error} = userSignin
+  const navigate = useNavigate()
 
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(signin(email, password))
   };
 
+  useEffect(() => {
+    if(userInfo){
+    navigate(redirect)}
+  }, [userInfo, redirect, navigate])
+
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Sign In</h1>
+          { loading && <LoadingBox></LoadingBox>}
+          {error &&  <MessageBox variant = "danger">{error}</MessageBox>}
         </div>
         <div>
           <label htmlFor="email">Email address</label>
